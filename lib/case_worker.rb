95e19356f53a6d4a69cc8e5749ce4811633b20f0
Neil Northrop <nnorthrop@gmail.com>
2015-06-02 15:40:47 -0700

    added CaseWorker case generating

--- lib/case_worker.rb
+++ lib/case_worker.rb
@@ -1,2 +1,33 @@
 require 'json'
 require 'bunny'
+
+class CaseWorker
+
+  def generate_case
+    {
+      case_id: generate_id,
+      is_med:  get_med,
+      status:  'new'
+    }
+  end
+
+  def generate_id
+    "#{rand}#{rand}"
+  end
+
+  def get_med
+    [true, false].sample
+  end
+
+  def publish(new_case)
+    puts "Published #{new_case[:case_id]}"
+  end
+
+end
+
+case_worker = CaseWorker.new
+10.times do
+  new_case = case_worker.generate_case
+  case_worker.publish(new_case)
+  sleep 10
+end
