e90f136480531ae195e7a5eb5aab54f1f8cb9e46
Jordan A. Skarbek <jaskarbek@gmail.com>
2015-06-10 15:13:31 -0700

    added history tracking to labs_worker

--- lib/labs_worker.rb
+++ lib/labs_worker.rb
@@ -17,7 +17,14 @@ class LabsWorker
     q.subscribe(block: true) do |delivery_info, properties, body|
       puts " [x] #{delivery_info.routing_key}:#{body}"
 
-      parsed_body    = JSON.parse(body, symbolize_names: true)
+      parsed_body = JSON.parse(body, symbolize_names: true)
+
+      parsed_body[:history] += {
+          worker_name: 'labs_worker',
+          timestamp:   Time.now,
+          message:     'Case picked up by labs_worker.'
+      }
+
       processed_body = get_med_info(parsed_body)
 
       publish(processed_body)
@@ -26,7 +33,12 @@ class LabsWorker
 
   def get_med_info(body)
     sleep 5
-    body[:status] = 'labs_processed'
+    body[:status]   = 'labs_processed'
+    body[:history] += {
+        worker_name: 'labs_worker',
+        timestamp:   Time.now,
+        message:     'Labs ordered.'
+    }
     return body
   end
 
@@ -37,6 +49,12 @@ class LabsWorker
 
     ch = conn.create_channel
     x  = ch.topic('new_cases')
+
+    new_case[:history] += {
+        worker_name: 'labs_worker',
+        timestamp:   Time.now,
+        message:     'Case sent to third_party_worker.'
+    }
 
     x.publish(new_case.to_json, routing_key: 'labs_processed')
 
